ab1936ef25ef18dbb82d6a983f96f3b8caa54c05
HarshulKachariya <harshulkachariya17@gmail.com>
2024-12-18 23:29:50 +0530

    fixed amplify issue

--- configureAmplify.js
+++ configureAmplify.js
@@ -1,3 +1,4 @@
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+import { Amplify } from "aws-amplify";
+import awsmobile from "./aws-exports";
+
+Amplify.configure(awsmobile);
